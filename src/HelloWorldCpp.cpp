c01ff5e328976da1148eee7802e9d1ebdc2a8ca1
abcworkspace <abcworkspace@gmail.com>
2015-09-01 00:56:52 -0400

    First Commit

---
+++ src/HelloWorldCpp.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorldCpp.cpp
+// Author      : Vu Huynh
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World in C++!!!" << endl;
+	return 0;
+}
